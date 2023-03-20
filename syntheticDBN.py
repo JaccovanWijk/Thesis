ecc4e462fccb86ebdc4511b8556175c0de7804a7
JaccovanWijk <jaccov.wijk@yahoo.com>
2023-03-20 15:00:48 +0100

    Add jupyter notebook

--- syntheticDBN.py
+++ syntheticDBN.py
@@ -4,6 +4,7 @@ import numpy as np
 import itertools
 import time
 import matplotlib.pyplot as plt
+import random
 
 import pyAgrum as gum
 # import pyAgrum.lib.dynamicBN as gdyn
@@ -31,6 +32,16 @@ class Bayes_Test:
             arcs = [arc for arc in list(itertools.combinations(all_nodes,2)) if (all_names[arc[0]][:-1] == all_names[arc[1]][:-1]) or (all_names[arc[0]][-1] == all_names[arc[1]][-1])]
 
             self.true_dbn.addArcs(arcs)
+
+        if type=='random':
+            # connect same nodes over time and different nodes within timestep
+            possible_arcs = [arc for arc in list(itertools.combinations(all_nodes,2))]
+            nr_connections = random.randint(0,len(possible_arcs))
+            print(f"{nr_connections}/{len(possible_arcs)} random arcs")
+            arcs = random.sample(possible_arcs,nr_connections)
+
+            self.true_dbn.addArcs(arcs)
+
         return 
 
 
@@ -39,9 +50,14 @@ class Bayes_Test:
         return
 
 
-    def generate_data(self,items=10000):
-        self.data,_=gum.generateSample(self.true_dbn,items,None,False)
-        self.data = self.data.reindex(sorted(self.data.columns, key=lambda x: x[::-1]), axis=1)
+    def generate_train_data(self,train_items=10000):
+        self.train_data,_=gum.generateSample(self.true_dbn,train_items,None,False)
+        self.train_data = self.train_data.reindex(sorted(self.train_data.columns, key=lambda x: x[::-1]), axis=1)
+        return
+    
+    def generate_test_data(self,test_items=10000):
+        self.test_data,_=gum.generateSample(self.true_dbn,test_items,None,False)
+        self.test_data = self.test_data.reindex(sorted(self.test_data.columns, key=lambda x: x[::-1]), axis=1)
         return
 
 
@@ -49,11 +65,11 @@ class Bayes_Test:
         discretizer = skbn.BNDiscretizer(defaultDiscretizationMethod='uniform',defaultNumberOfBins=5,discretizationThreshold=25)
         # Create nodes
         template = gum.BayesNet()
-        for name in self.data:
-            template.add(discretizer.createVariable(name, self.data[name]))
+        for name in self.train_data:
+            template.add(discretizer.createVariable(name, self.train_data[name]))
 
         # Set up learner
-        learner = gum.BNLearner(self.data, template)
+        learner = gum.BNLearner(self.train_data, template)
         # TODO: More structure and parameter algorithms
         if structure == 'hill':
             learner.useGreedyHillClimbing()
@@ -61,8 +77,8 @@ class Bayes_Test:
             print("parameter algorithm")
 
         # Force no back in time arcs
-        for name1 in self.data:
-            for name2 in self.data:
+        for name1 in self.train_data:
+            for name2 in self.train_data:
                 if int(name1[-1]) > int(name2[-1]):
                     learner.addForbiddenArc(name1,name2)
                 if int(name1[-1]) + timesteps <= int(name2[-1]):
@@ -79,14 +95,14 @@ class Bayes_Test:
         return
 
 
-    def score_classifier(self):
+    def score_classifier(self, test_items=1000):
         self.ClassfromBN.fromTrainedModel(self.true_dbn,targetAttribute=self.target)
-        self.generate_data()
-        xTest, yTest = self.data.loc[:, self.data.columns != self.target], self.data[self.target]
+        self.generate_test_data(test_items=test_items)
+        xTest, yTest = self.test_data.loc[:, self.test_data.columns != self.target], self.test_data[self.target]
         return self.ClassfromBN.score(xTest, yTest.apply(lambda x: x==1))
 
     
-    def time_test(self, timesteps=2, cpt_repetitions=5, data_repetitions=10, targets=["c2"]):
+    def time_test(self, timesteps=2, cpt_repetitions=5, data_repetitions=10, targets=["c2"], train_items=10000, test_items=1000):
         all_scores = pd.DataFrame(columns=targets)
         for i in range(cpt_repetitions):
             self.generate_CPTs()
@@ -95,36 +111,35 @@ class Bayes_Test:
 
             for j in range(data_repetitions):
                 # print(f"Cpt {i + 1} and Data {j + 1}")
-                self.generate_data()
+                self.generate_train_data(train_items=train_items)
                 bn = self.learn_dbn(timesteps=timesteps)
 
                 scores = {}
                 for target in targets:
-                    bayestest.classifier_from_dbn(bn, target)
-                    scores[target] = [bayestest.score_classifier()]
-                all_scores = pd.concat([all_scores, pd.DataFrame(scores)], ignore_index=True)
+                    self.classifier_from_dbn(bn, target)
+                    scores[target] = [self.score_classifier(test_items=test_items)]
+                all_scores = pd.concat([all_scores, pd.DataFrame(scores, index=[f'{i+1}.{j+1    }'])])#, ignore_index=True)
 
         return all_scores
 
+if __name__ == '__main__':
+    print("begin")
+    start_time = time.time()
 
+    timesteps = 3
+    # nodes = 4
+    bayestest = Bayes_Test(timesteps=timesteps) 
+    print("Created")
 
-print("begin")
-start_time = time.time()
+    targets = list(sorted(bayestest.true_dbn.names(), key=lambda x: x[::-1]))[-3:]
 
-timesteps = 3
-# nodes = 4
-bayestest = Bayes_Test(timesteps=timesteps) 
-print("Created")
+    fig, axs = plt.subplots(1, timesteps)
 
-targets = list(sorted(bayestest.true_dbn.names(), key=lambda x: x[::-1]))[-3:]
+    for timesteps in range(1, timesteps + 1):
+        scores = bayestest.time_test(targets=targets, timesteps=timesteps)
 
-fig, axs = plt.subplots(1, timesteps)
+        print("--- %s seconds ---" % (time.time() - start_time))
 
-for timesteps in range(1, timesteps + 1):
-    scores = bayestest.time_test(targets=targets, timesteps=timesteps)
+        axs[timesteps-1].boxplot(scores)
 
-    print("--- %s seconds ---" % (time.time() - start_time))
-
-    axs[timesteps-1].boxplot(scores)
-
-plt.show()
+    plt.show()
